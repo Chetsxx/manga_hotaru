6886c0bcdfaae0f8bd1d52e114879ff14a6c1555
Chetna Rawat <chetnarawat3@gmail.com>
2023-01-29 16:17:08 +0530

    Initial Commit

---
+++ main.py
@@ -0,0 +1,48 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import os
+import requests
+import time
+
+FOLDER = "C:\\Users\Dell\Downloads\st"
+os.makedirs(FOLDER, exist_ok=True)
+
+options = webdriver.ChromeOptions()
+options.add_experimental_option("detach", True)
+
+service = Service("C:\\Users\Dell\Development\chromedriver.exe")
+driver = webdriver.Chrome(options=options, service=service)
+driver.maximize_window()
+
+driver.get("https://urasunday.com/title/2169/200736")
+driver.refresh()
+time.sleep(10)
+
+image = []
+
+for x in range(50):
+    time.sleep(2)
+    image = driver.find_elements(By.CLASS_NAME, "zao-image")
+    next = driver.find_element(By.CSS_SELECTOR, "body")
+    next.send_keys(Keys.LEFT)
+
+
+for x in image:
+    url = x.get_attribute("src")
+    file = url.split("/")[-1]
+    filename = file.split("?")[0]
+    print('url:', url)
+    print('filename:', filename)
+    print('-----')
+    full_path = os.path.join(FOLDER, filename)
+    print(full_path)
+
+    response = requests.get(url)
+    data = response.content
+    with open(full_path, "wb") as fh:
+        fh.write(response.content)
+
+
+
